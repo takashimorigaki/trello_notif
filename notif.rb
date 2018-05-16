cd09464943674401a7c97891e083c7cb8b4793ea
takashimorigaki <takashi.morigaki@8ce.me>
2018-05-16 14:28:50 +0900

    Enable to manage multiple List

--- notif.rb
+++ notif.rb
@@ -5,7 +5,7 @@ require 'date'
 
 # trello
 BOARD_ID = '<board_id>'
-LIST_NAME = '<list_name>'
+LIST_NAMES = ['<list_name1>', '<list_name2>', '<list_name3>']
 CONSUMER_KEY = '<trello_key>'
 CONSUMER_SECRET = '<trello_secret>'
 OAUTH_TOKEN = '<trello_token>'
@@ -33,19 +33,21 @@ end
 
 board = Trello::Board.find(BOARD_ID)
 
-list_id = nil
+list_ids = []
 
 board.lists.each do |l|
-  list_id = l.id if l.name == LIST_NAME
+  list_ids.push(l.id) if LIST_NAMES.include?(l.name)
 end
 
-exit if list_id.nil?
+exit if list_ids.nil?
 
-list = Trello::List.find(list_id)
-list.cards.each do |c|
-  if c.due.present? and c.due.to_date == Date.today + 1
-    text = "#{list.name}の「#{c.name}」の期限は#{c.due.to_date.to_s.gsub('-', '/')}までだよ"
-    print "#{text}\n"
-    notify_to_slack(text)
+list_ids.each do |l_id|
+  list = Trello::List.find(l_id)
+  list.cards.each do |c|
+    if c.due.present? and c.due.to_date == Date.today + 1
+      text = "#{list.name}の「#{c.name}」の期限は#{c.due.to_date.to_s.gsub('-', '/')}までだよ"
+      print "#{text}\n"
+      notify_to_slack(text)
+    end
   end
 end
